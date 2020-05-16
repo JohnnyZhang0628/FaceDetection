0c3c6803439a38de6f84ad3201beceb826bfa8d3
JohnnyZhang0628 <237183141@qq.com>
2020-05-16 18:55:28 +0800

    添加项目文件。

---
+++ FaceDetection/FrmMain.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FaceDetection
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.picCamer = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCamer)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Haar人脸识别";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnHaarFaceDetection);
+            // 
+            // picCamer
+            // 
+            this.picCamer.Location = new System.Drawing.Point(6, 20);
+            this.picCamer.Name = "picCamer";
+            this.picCamer.Size = new System.Drawing.Size(560, 479);
+            this.picCamer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picCamer.TabIndex = 1;
+            this.picCamer.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.picCamer);
+            this.groupBox1.Location = new System.Drawing.Point(192, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(580, 509);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "视频区域";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "CNN人脸识别";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnCNNFaceDetection);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 71);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Lbp人脸识别";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnLbpFaceDetection);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmMain";
+            this.Text = "人脸识别";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnFormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.picCamer)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picCamer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FaceDetection/FrmMain.cs
@@ -0,0 +1,145 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Dnn;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.XFeatures2D;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FaceDetection
+{
+    public partial class FrmMain : Form
+    {
+        private bool openCamera = false;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnHaarFaceDetection(object sender, EventArgs e)
+        {
+            openCamera = false;
+            var haarCascade = new CascadeClassifier("model/haarcascade_frontalface_alt.xml");
+            Run(src => HaarAndLbp(src,haarCascade));
+        }
+
+        private void OnLbpFaceDetection(object sender, EventArgs e)
+        {
+            openCamera = false;
+            var lbpCascade = new CascadeClassifier("model/lbpcascade_frontalface.xml");
+           
+            Run(src => HaarAndLbp(src,lbpCascade));
+        }
+
+        private void OnCNNFaceDetection(object sender, EventArgs e)
+        {
+            openCamera = false;
+            Run(src => CNN(src));
+        }
+        private void Run(Func<Mat, Mat> src)
+        {
+            openCamera = true;
+            VideoCapture capture = new VideoCapture(0);
+            using (Mat image = new Mat()) // Frame image buffer
+            {
+                // When the movie playback reaches end, Mat.data becomes NULL.
+                while (openCamera)
+                {
+                    capture.Read(image); // same as cvQueryFrame
+                    if (image.Empty()) break;
+
+                    picCamer.Image = src.Invoke(image).ToBitmap();
+                    Cv2.WaitKey(30);
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        ///<param name="src"></param>
+        /// <param name="cascade"></param>
+        /// <returns></returns>
+        private Mat HaarAndLbp(Mat src,CascadeClassifier cascade)
+        {
+          
+
+            using (var gray = new Mat())
+            {
+                Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+
+                // Detect faces
+                Rect[] faces = cascade.DetectMultiScale(
+                    gray, 1.08, 2, HaarDetectionType.ScaleImage, new OpenCvSharp.Size(30, 30));
+
+                // Render all detected faces
+                foreach (Rect face in faces)
+                {
+                    src.Rectangle(face, Scalar.Red, 2);
+                }
+            }
+            return src;
+        }
+
+
+        private Mat CNN(Mat src)
+        {
+            const string configFile = "model/deploy.prototxt";
+            const string faceModel = "model/res10_300x300_ssd_iter_140000_fp16.caffemodel";
+
+            // Read sample image
+            int srcHeight = src.Rows;
+            int srcWidth = src.Cols;
+            using (var faceNet = CvDnn.ReadNetFromCaffe(configFile, faceModel))
+            {
+                using (var blob = CvDnn.BlobFromImage(src, 1.0, new OpenCvSharp.Size(300, 300),
+                    new Scalar(104, 117, 123), false, false))
+                {
+                    faceNet.SetInput(blob, "data");
+
+                    using (var detection = faceNet.Forward("detection_out"))
+                    {
+                        using (var detectionMat = new Mat(detection.Size(2), detection.Size(3), MatType.CV_32F,
+                          detection.Ptr(0)))
+                        {
+                            for (int i = 0; i < detectionMat.Rows; i++)
+                            {
+                                float confidence = detectionMat.At<float>(i, 2);
+
+                                if (confidence > 0.7)
+                                {
+                                    int x1 = (int)(detectionMat.At<float>(i, 3) * srcWidth);
+                                    int y1 = (int)(detectionMat.At<float>(i, 4) * srcHeight);
+                                    int x2 = (int)(detectionMat.At<float>(i, 5) * srcWidth);
+                                    int y2 = (int)(detectionMat.At<float>(i, 6) * srcHeight);
+
+                                    Cv2.Rectangle(src, new OpenCvSharp.Point(x1, y1), new OpenCvSharp.Point(x2, y2), new Scalar(0, 255, 0), 2,
+                                        LineTypes.Link4);
+                                }
+                            }
+
+                            return src;
+                        }
+                    }
+
+
+                }
+
+
+            }
+
+        }
+
+        private void OnFormClosing(object sender, FormClosingEventArgs e)
+        {
+            openCamera = false;
+        }
+
+
+    }
+}
